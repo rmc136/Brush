290c0c227692e53f4eb902d3dca9cb34ce94ee30
Joao Ferreira <joao.ferreira.14.7a@gmail.com>
2025-09-19 01:23:48 +0100

    added icon, need to start main screen, and do AI animations

--- Brush/brush-client/src/main/java/com/mybrushgame/client/desktop/DesktopLauncher.java
+++ Brush/brush-client/src/main/java/com/mybrushgame/client/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Brush Card Game");
+        config.setWindowIcon("icon.png");
         config.setWindowedMode(1024, 768);
         new Lwjgl3Application(new BrushGameUI(), config);
     }
